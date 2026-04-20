b5c542215345cd9ba9b1a47e3f20a297ff4d7d75
CruidGals <kchiem30@gmail.com>
2026-04-20 15:41:38 -0400

    Begin personal website v2

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section className="flex h-dvh w-full flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold">Hello World</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
